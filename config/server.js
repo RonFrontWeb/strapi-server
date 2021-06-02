eeef54a30db2d99bc0d0e4593116eb3f72ee7b57
RonFrontWeb <62059074+RonFrontWeb@users.noreply.github.com>
2021-06-02 14:59:01 +0200

    Test

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '54833836cb33c0f36f002ecc8b0b0595'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6573934c-6ed0-41d5-8fe3-5299a718aa38'
+};
